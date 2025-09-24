d9f29ab92b013f383e5efc7faf43182d91c1f88e
MVP2333448 <phu.mach2333448@hcmut.edu.vn>
2025-09-24 15:23:26 +0700

    week 4

--- backend/src/modules/test/test.service.ts
+++ backend/src/modules/test/test.service.ts
@@ -1,13 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Metadata, MetadataDocument } from 'src/schemas/metadata.schema';
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 @Injectable()
 export class TestService {
-    constructor(private readonly httpService: HttpService) { }
+    constructor(
+        private readonly httpService: HttpService,
+        @InjectModel(Metadata.name) private metadataModel: Model<MetadataDocument>,
+    ) { }
+
+    private genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+    async askAI(prompt: string) {
+        const model = this.genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const result = await model.generateContent(prompt);
+        return result.response.text();
+    }
+
+    async create(obj: { chunks: number; embedding: number[]; text: string }) {
+        const newMetadata = new this.metadataModel(obj);
+        return newMetadata.save();
+    }
+
+    async queryVector(vector: number[], topK: number) {
+        const result = await this.metadataModel.aggregate([
+            {
+                $vectorSearch: {
+                    index: 'vector_index',
+                    path: 'embedding',
+                    queryVector: vector,
+                    numCandidates: 10,
+                    limit: topK,
+                }
+            }
+        ]);
+        return result;
+    }
 
     async embeddings(chunks: string[]) {
-        const url = 'https://fbf3bed69cc8.ngrok-free.app/embed';
+        // const url = 'https://672d4c5fe586.ngrok-free.app/embed';
+        const url = process.env.API_EMBEDDINGS;
+
         const response = await firstValueFrom(this.httpService.post(url, { texts: chunks }));
         return response.data.embeddings;
     }
---
+++ backend/src/schemas/metadata.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type MetadataDocument = Metadata & Document;
+
+@Schema()
+export class Metadata {
+    @Prop({ required: true })
+    chunks: number;
+
+    @Prop({ required: true })
+    embedding: number[];
+
+    @Prop({ required: true })
+    text: string;
+}
+
+export const MetadataSchema = SchemaFactory.createForClass(Metadata);
