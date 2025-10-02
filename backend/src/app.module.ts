b2ec4aacfee5af76b8036fde4fe76db8e2b663e7
LamNguyen1407 <ln14072004@gmail.com>
2025-10-02 10:12:38 +0700

    fix: add mongo uri in env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './modules/test/test.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://machvanphu1999_db_user:pfs0902919129@cluster0.cfiprv5.mongodb.net/chatbot?retryWrites=true&w=majority&appName=Cluster0'),
     ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      })
+    }),
     TestModule,
   ],
   controllers: [AppController],
