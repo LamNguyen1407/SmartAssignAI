74e2ebcef7d6a2a3f8ea578be2106bedca071178
LamNguyen1407 <ln14072004@gmail.com>
2025-11-13 09:44:04 +0700

    signin login

---
+++ frontend/src/app/register/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { RegisterForm } from "@/components/auth/RegisterForm"
+
+const RegisterPage = () => {
+  return (
+    <RegisterForm />
+  )
+}
+
+export default RegisterPage
