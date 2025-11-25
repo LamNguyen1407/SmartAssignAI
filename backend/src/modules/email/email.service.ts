70c3f50e242ba7f77135933953fe8dc55650fd06
LamNguyen1407 <ln14072004@gmail.com>
2025-11-25 13:07:07 +0700

    forgot reset password

--- backend/src/modules/email/email.service.ts
+++ backend/src/modules/email/email.service.ts
@@ -6,21 +6,25 @@ export class EmailService {
     private transporter;
 
     constructor() {
+        const isProd = process.env.NODE_ENV === 'production';
         this.transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
+            host: process.env.SMTP_HOST || 'smtp.ethereal.email',
             port: process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT) : 587,
             secure: false, // true for 465, false for other ports
             auth: {
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS,
+                user: process.env.MAIL_USER || 'chasity55@ethereal.email',
+                pass: process.env.MAIL_PASS || 'xA1v1fvEhAuQSN4w95',
             },
+            tls: isProd ? undefined : {
+                rejectUnauthorized: false
+            }
         });
     }
 
     async sendMail(to: string, subject: string, html: string) {
         try{
             await this.transporter.sendMail({
-                from: `"SmartAssignAI" System`,
+                from: `SmartAssignAI System`,
                 to,
                 subject,
                 html,
